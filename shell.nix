d59343dad8dca5b90d56702e50536ba200988743
Izzy Lancaster <avrisaac555@gmail.com>
2021-10-04 18:53:50 -0700

    Added shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+  ];
+} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.rust-bin.stable.latest.default.override {
+      extensions = ["rust-src"];
+    })
+  ];
+}
