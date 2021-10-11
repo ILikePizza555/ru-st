a9c26d40b48588dd9f688c414ecc2545a9778720
Izzy Lancaster <avrisaac555@gmail.com>
2021-10-11 12:20:03 -0700

    Refactored some of the setup code into a separate function

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use x11rb::connection::Connection;
 use x11rb::protocol::xproto::*;
 use x11rb::COPY_DEPTH_FROM_PARENT;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (connection, screen_num) = x11rb::connect(None).unwrap();
+pub fn initialize_x11(connection: &impl Connection, screen_num: usize) -> Result<(), Box<dyn std::error::Error>> {
     let screen = &connection.setup().roots[screen_num];
     let window_id = connection.generate_id()?;
 
@@ -21,7 +20,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &CreateWindowAux::new().background_pixel(screen.white_pixel))?;
 
     connection.map_window(window_id)?;
-    connection.flush();
+    Ok(connection.flush()?)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (connection, screen_num) = x11rb::connect(None).unwrap();
+    initialize_x11(&connection, screen_num)?;
 
     loop {
         println!("Event: {:?}", connection.wait_for_event()?);
